27eff1f16a76de0a07eecb878b2d6276df1ceb50
fbrcode <fabio.bressler@gmail.com>
2022-09-28 20:45:51 -0300

    load from cached data for bulk insert

---
+++ src/loadFromCache.ts
@@ -0,0 +1,45 @@
+import { readdirSync, existsSync } from 'fs';
+import config from './config';
+import { readFile, humanNumber } from './utils';
+import { swapHistoryBulkInsert } from './dbStream';
+import { logger } from './logger';
+
+export function cachedEventsExists(poolId: string) {
+  const fileSourceFullPath = `${config.CACHE_DIRECTORY}/${poolId}-swap-events.json`;
+  return existsSync(fileSourceFullPath);
+}
+
+export async function loadFromCachedData(poolId?: string) {
+  const cachedFiles = readdirSync(config.CACHE_DIRECTORY);
+  const eventsCachedFiles = cachedFiles.filter(
+    (f) => f.toLocaleLowerCase().startsWith('0x') && f.toLocaleLowerCase().endsWith('events.json')
+  );
+  for (const fileSource of eventsCachedFiles) {
+    const pool = fileSource.substring(0, fileSource.indexOf('-'));
+    logger.info(`Processing pool ${pool}`);
+    logger.info(`Loading data from cached: ${fileSource}...`);
+    const fileSourceFullPath = `${config.CACHE_DIRECTORY}/${fileSource}`;
+    try {
+      const data = readFile(fileSourceFullPath);
+      const events = JSON.parse(data);
+      let eventList = [];
+      events['EventBody'].map((e: any) => {
+        eventList.push({
+          blockNumber: e.blockNumber,
+          timestamp: e.timestamp,
+          amount0: e.data.amount0,
+          amount1: e.data.amount1,
+          tick: e.data.tick,
+          sqrtPriceX96: e.data.sqrtPriceX96,
+        });
+      });
+      logger.debug(`Generated ${humanNumber(eventList.length)} event items to load`);
+      // refresh and add data again from cache...
+      swapHistoryBulkInsert(pool, eventList);
+    } catch (e) {
+      logger.error(e);
+    }
+  }
+}
+
+loadFromCachedData();
