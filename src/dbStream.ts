d83e7071a9808dcaf2ffb2069dce546984af17c3
fbrcode <fabio.bressler@gmail.com>
2022-09-29 07:30:31 -0300

    add transaction hash to storage

--- src/dbStream.ts
+++ src/dbStream.ts
@@ -32,7 +32,7 @@ export function swapHistoryBulkInsert(poolId: string, dataArray: any[]) {
   pool.connect(function (err, client, done) {
     const stream = client.query(
       pgCopy.from(
-        `COPY ${targetTable} (pool_id, block_number, unix_timestamp, hist_timestamp, amount0, amount1, tick, sqrt_price_x96) FROM STDIN DELIMITER E'\t' CSV`
+        `COPY ${targetTable} (pool_id, block_number, unix_timestamp, hist_timestamp, transaction_hash, amount0, amount1, tick, sqrt_price_x96) FROM STDIN DELIMITER E'\t' CSV`
       )
     );
     stream.on('error', (e) => logger.error(e));
@@ -43,7 +43,7 @@ export function swapHistoryBulkInsert(poolId: string, dataArray: any[]) {
       .filter((e) => !!e.timestamp) // skip if timestamp is not found
       .map((d) => {
         const ts = new Date(d.timestamp * 1000).toString().substring(0, 24);
-        const row = `${poolId}\t${d.blockNumber}\t${d.timestamp}\t${ts}\t${d.amount0}\t${d.amount1}\t${d.tick}\t${d.sqrtPriceX96}`;
+        const row = `${poolId}\t${d.blockNumber}\t${d.timestamp}\t${ts}\t${d.txHash}\t${d.amount0}\t${d.amount1}\t${d.tick}\t${d.sqrtPriceX96}`;
         readable.push(row + '\n');
       });
     readable.push(null);
